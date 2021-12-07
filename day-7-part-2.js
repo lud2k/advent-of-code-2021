95374b61d4059956b1f5620fd345836411bca990
lud2k <ludovic.cabre@gmail.com>
2021-12-07 16:27:51 -0500

    Create day-7-part-2.js

---
+++ day-7-part-2.js
@@ -0,0 +1,18 @@
+const day7part2 = async () => {
+  const response = await fetch('https://adventofcode.com/2021/day/7/input')
+  const data = await response.text()
+  const positions = data.split('\n')[0].split(',').map(n => parseInt(n, 10))
+  const min = positions.reduce((ret, pos) => Math.min(ret, pos), positions[0])
+  const max = positions.reduce((ret, pos) => Math.max(ret, pos), positions[0])
+  const costs = {}
+  for (let i=min; i<=max; i++) {
+    costs[i] = positions.reduce((ret, pos) => {
+      const dist = Math.abs(pos-i)
+      return ret + (dist ? parseInt((Math.pow(dist+1,2) - dist) / 2) : 0)
+    }, 0)
+  }
+  const entry = Object.entries(costs).sort((e1, e2) => e1[1]-e2[1])[0]
+  console.log('result', entry[1])
+}
+
+day7part2()
